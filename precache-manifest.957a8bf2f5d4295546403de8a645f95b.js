406401a4b25bc25841e5b5e98355231666b901cf
Anish Aggarwal <anish17122000@gmail.com>
2019-10-03 12:23:13 +0530

    Updates

---
+++ precache-manifest.957a8bf2f5d4295546403de8a645f95b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a9258e45cf2712c31ab308a62d85036",
+    "url": "/react-items/index.html"
+  },
+  {
+    "revision": "030a058f019c9e9184f5",
+    "url": "/react-items/static/css/main.de7c598b.chunk.css"
+  },
+  {
+    "revision": "4c50a70d40f214572ecc",
+    "url": "/react-items/static/js/2.68e73e33.chunk.js"
+  },
+  {
+    "revision": "030a058f019c9e9184f5",
+    "url": "/react-items/static/js/main.93a1d3d6.chunk.js"
+  },
+  {
+    "revision": "5da6a3e17d2e118aa3ec",
+    "url": "/react-items/static/js/runtime-main.32345171.js"
+  }
+]);
